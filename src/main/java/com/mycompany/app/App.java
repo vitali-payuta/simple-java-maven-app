30d1328bce16327cda55f502ed2f2240ad77ae19
Vitali Payuta <kudzo.san@gmail.com>
2022-03-15 14:04:37 +0300

    q test2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,11 +8,7 @@ package com.mycompany.app;
 
 public class App
 {
-    /*public void infiniteLoopUsingWhile() {
-        while (true) {
-        // do something
-        }
-    } */
+   
     
     private final String message = "Hello World!";
 
